de16223bf74160d56dae3cab4e958a7cdd13f2f3
Mykola Fryziuk <kolyenkonexsus@gmail.com>
2023-02-20 20:14:19 +0200

    Add dfs algorithm and directed graph

--- baseline_algorithms.py
+++ baseline_algorithms.py
@@ -1,14 +1,54 @@
-def bfs(graph, root):
+from topology import Graph
+
+
+def bfs(graph: Graph, root: int):
     queue = []
     discovered = [False] * len(graph.data)
+    distance = [-1] * len(graph.data)  # Mark unreachable nodes as -1
+    previous = [-1] * len(graph.data)
 
     discovered[root] = True
-    queue.append()
+    queue.append(root)
+    distance[root] = 0
+    idx = 0
+
+    while idx < len(queue):
+        current = queue[idx]
+        idx += 1
+
+        for neighbour, dist in enumerate(graph.data[current]):
+            if dist > 0 and not discovered[neighbour]:
+                queue.append(neighbour)
+                discovered[neighbour] = True
+                distance[neighbour] = distance[current] + dist
+                previous[neighbour] = current
+
+    return queue, previous, distance
 
 
-def dfs(topology):
-    best_route = []
-    return best_route
+# Useless for getting shortest path
+def dfs(graph: Graph, root: int):
+    stack = [root]
+    discovered = [False] * len(graph.data)
+    previous = [-1] * len(graph.data)
+    distance = [-1] * len(graph.data)
+
+    discovered[root] = True
+    distance[root] = 0
+
+    history = []
+    while stack:
+        current = stack.pop()
+        history.append(current)
+
+        for neighbour, dist in enumerate(graph.data[current]):
+            if dist > 0 and not discovered[neighbour]:
+                stack.append(neighbour)
+                discovered[neighbour] = True
+                previous[neighbour] = current
+                distance[neighbour] = distance[current] + dist
+
+    return history, previous, distance
 
 
 def dijkstra(topology):
--- topology.py
+++ topology.py
@@ -1,10 +1,15 @@
+import baseline_algorithms
+
+
 class Graph:
-    def __init__(self, num_nodes, edges):
+    def __init__(self, num_nodes, edges, is_directed=False):
+        self.is_directed = is_directed
         self.num_nodes = num_nodes
         self.data = [[0] * num_nodes for _ in range(num_nodes)]
-        for n1, n2 in edges:
-            self.data[n1][n2] = 1
-            self.data[n2][n1] = 1
+        for n1, n2, weight in edges:
+            self.data[n1][n2] = weight
+            if not is_directed:
+                self.data[n2][n1] = weight
 
     def __repr__(self):
         header = '    '
@@ -16,15 +21,17 @@ class Graph:
     def __str__(self):
         return self.__repr__()
 
-    def add_edge(self, edge: tuple[int, int]):
-        n1, n2 = edge
-        self.data[n1][n2] = 1
-        self.data[n2][n1] = 1
+    def add_edge(self, edge: tuple[int, int, int]):
+        n1, n2, weight = edge
+        self.data[n1][n2] = weight
+        if not self.is_directed:
+            self.data[n2][n1] = weight
 
     def remove_edge(self, edge: tuple[int, int]):
         n1, n2 = edge
         self.data[n1][n2] = 0
-        self.data[n2][n1] = 0
+        if not self.is_directed:
+            self.data[n2][n1] = 0
 
 
 def main():
@@ -34,6 +41,7 @@ def main():
     graph.add_edge((2, 2))
     graph.remove_edge((0, 3))
     print(graph)
+    print(baseline_algorithms.dfs(graph, 0))
 
 
 if __name__ == '__main__':
